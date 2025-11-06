92e5463814b37d4f6947a26deb95575981e0e84e
Maciej <metro1568@gmail.com>
2025-11-06 15:02:12 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,54 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+const { generateRandomString } = require('../support/commands');
 
-  });
-});
+describe('Sign In page',
+  {
+    defaultBrowser: 'chrome',
+    viewportHeight: 900,
+    viewportWidth: 1200,
+    scrollBehavior: 'center'
+  },
+  () => {
+    before(() => {
+      cy.visit('user/login');
+    });
+
+    it('should provide an ability to log in', () => {
+      const username = generateRandomString(8, '', true, false);
+      const email = generateRandomString(7, '', false, false) + '@gmail.com';
+      const password = generateRandomString(10, '%$', true, true);
+      let createdUser;
+
+      cy.request({
+        method: 'Post',
+        url: 'https://conduit.mate.academy/api/users',
+        body: {
+          user: {
+            username,
+            email,
+            password
+          }
+        }
+      })
+        .then((response) => {
+          createdUser = { ...response.body.user };
+          cy.get('[placeholder="Email"]')
+            .type(email);
+          cy.get('[placeholder="Password"]')
+            .type(password);
+          cy.get('form button[type="submit"]')
+            .click();
+          cy.location('href')
+            .should('equal', 'https://conduit.mate.academy/');
+          cy.get('.nav-link[href*="/profile/"]')
+            .should((user) => {
+              const displayedUsernameOnPage = user[0].getAttribute('href');
+              const createdUsername = createdUser.username;
+
+              expect(displayedUsernameOnPage).to.include(createdUsername);
+            });
+        });
+    });
+  }
+);
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,85 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * @param {number} length
+ * @param {string} specialCharacter
+ * @param {boolean} randomUpperCase
+ * @param {boolean} oneUpperCase
+ * @returns {string}
+ */
+
+function generateRandomString
+(length = 8, specialCharacter = '~`#$%^&*()_+=',
+  randomUpperCase = true, oneUpperCase = true) {
+  const haveMinLength = length > 3;
+  const hasSpecialCharacter = (typeof specialCharacter === 'string')
+    ? specialCharacter.match(/[^\w\d\s\p{Letter}@]/gu) !== null
+    : false;
+  const properSpecialCharacters = hasSpecialCharacter
+    ? specialCharacter.match(/[^\w\d\s\p{Letter}@]/gu).join('')
+    : false;
+  const hasRandomUpperCase = randomUpperCase === true;
+  let howManySpecialCharacter;
+  let generatedUserName = '';
+
+  if (!haveMinLength) {
+    return new Error(`Username has less than 4 letters`);
+  }
+
+  if (hasSpecialCharacter === true) {
+    const balance = length - properSpecialCharacters.length;
+
+    switch (Math.sign(balance)) {
+      case 1:
+        howManySpecialCharacter = properSpecialCharacters.length;
+
+        break;
+      case 0:
+        howManySpecialCharacter = properSpecialCharacters.length - 1;
+
+        break;
+      case -1:
+        howManySpecialCharacter =
+            properSpecialCharacters.length - (Math.abs(balance) + 1);
+
+        break;
+    };
+  } else {
+    howManySpecialCharacter = 0;
+  }
+
+  const letters = (() => {
+    const lowerCase = 'qwertyuioplkjhgfdsazxcvbnm';
+
+    if (hasRandomUpperCase === true) {
+      const upperCase = lowerCase.toUpperCase();
+
+      return [...lowerCase, ...upperCase];
+    }
+
+    return [...lowerCase];
+  })();
+  const loopEnd1 = length - howManySpecialCharacter;
+
+  for (let i = 0; i < loopEnd1; i++) {
+    const letterScope = letters.length - 1; // indeksy
+    const randomLetterIndex = Math.floor(Math.random() * letterScope); // indeksy
+
+    if (i === 0 && oneUpperCase === true) {
+      generatedUserName += letters[randomLetterIndex].toUpperCase();
+    } else {
+      generatedUserName += letters[randomLetterIndex];
+    }
+  }
+
+  if (hasSpecialCharacter === true) {
+    for (let i = loopEnd1, i2 = 0; i < length; i++, i2++) {
+      generatedUserName += properSpecialCharacters[i2];
+    }
+  }
+
+  return generatedUserName;
+}
+
+module.exports = { generateRandomString };
